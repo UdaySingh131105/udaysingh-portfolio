e15f30996eb77e785b98d9845ef06b7ababed00b
Uday Singh <udaysingh131105@gmail.com>
2025-09-25 01:10:38 +0530

    created a nodemailer api

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  try {
+    const { name, lastname, email, message } = await req.json();
+
+    console.log(process.env.EMAIL_SENDER, process.env.EMAIL_PASS, process.env.EMAIL_USER, " env");
+    
+    const transporter = nodemailer.createTransport({
+      service: "gmail", 
+      auth: {
+        user: process.env.EMAIL_SENDER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `New Contact Form Submission from ${name} ${lastname}`,
+      html: `
+        <h2>New Message from Contact Form</h2>
+        <p><b>Name:</b> ${name} ${lastname}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Message:</b><br/>${message}</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return NextResponse.json({ success: false, message: "Error sending email" }, { status: 500 });
+  }
+}
